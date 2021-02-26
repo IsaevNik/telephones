e9646c4a4bdb5b1fad71d7c360431151f3581c75
Никита Исаев <nikita.isaev@corp.mail.ru>
2021-02-26 10:31:21 +0300

    add timeouterror handling

--- README.md
+++ README.md
@@ -8,7 +8,11 @@
     cp test.csv sites.csv
     ```
 
-3) Run container:
+3) Run container with file input:
     ```
     docker run --rm -v $(pwd)/sites.csv:/usr/src/telephones/sites.csv -t telephones:latest -f "sites.csv"
+    ```
+    or with a list of urls as command's arguments
+    ```
+    docker run --rm -t telephones:latest "https://masterdel.ru" "https://repetitors.info"
     ```
--- telephone_parser.py
+++ telephone_parser.py
@@ -1,19 +1,18 @@
 import argparse
+import sys
 import asyncio
 import csv
 import re
-from collections.abc import Iterable
 from typing import List
 
 import aiohttp
 import logging
 
-
 REQUEST_TIMEOUT = aiohttp.ClientTimeout(total=2.0)
 
 
 telephone_regexp = re.compile(
-    r'[8|+7]\s?\(?\s?\d{3}\s?\)?\s?\d{3}\s?-?\s?\d{2}\s?-?\s?\d{2}|\d{3}\s?-\s?\d{2}\s?-\s?\d{2}'
+    r'8\s?\(?\s?\d{3}\s?\)?\s?\d{3}\s?-?\s?\d{2}\s?-?\s?\d{2}|\d{3}\s?-\s?\d{2}\s?-\s?\d{2}'
 )
 
 
@@ -32,30 +31,35 @@ def get_result_by_regexp(regexp: re.Pattern, text: str) -> List[str]:
 
 async def get_html(url: str) -> str:
     async with aiohttp.ClientSession(timeout=REQUEST_TIMEOUT) as session:
-        async with session.get(url) as response:
+        async with session.get(url, raise_for_status=True) as response:
             return await response.text()
 
 
-async def parse_telephone_number_from_site(url: str) -> List[str]:
+async def parse_telephone_number_from_site(url: str) -> (List[str], str):
     try:
         html = await get_html(url)
-    except aiohttp.ClientError as err:
-        logging.error('%s request error %s', url, err)
-        return []
+    except (aiohttp.ClientError, asyncio.TimeoutError) as exc:
+        logging.error('%s request error %s', url, exc)
+        return [], str(exc)
 
-    result = get_result_by_regexp(telephone_regexp, html)
-    return list(set(result))
+    try:
+        result = get_result_by_regexp(telephone_regexp, html)
+    except Exception as exc:
+        logging.error('%s parse error %s', url, exc)
+        return [], str(exc)
+
+    return list(set(result)), ''
 
 
-async def parse_telephone_numbers(urls):
+async def parse_telephone_numbers(urls: List[str]):
     results = await asyncio.gather(*[
         parse_telephone_number_from_site(url) for url in urls
     ], return_exceptions=True)
-
-    for i, phones in enumerate(results):
-        if not isinstance(phones, Iterable):
-            phones = []
-        print(f'{urls[i]}: {phones}')
+    for i, (phones, error) in enumerate(results):
+        if error:
+            sys.stderr.write('{}: error: {}\n'.format(urls[i], error))
+        else:
+            sys.stdout.write(f'{urls[i]}: {phones}\n')
 
 
 if __name__ == '__main__':
